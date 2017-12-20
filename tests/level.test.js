0b29a2357eba477202e594fb5bb67a3b20ee6301
lsphillips <lsphillips.mail@gmail.com>
2017-12-20 14:46:49 +0000

    Fixed all the ESLint issues raised with the Level unit tests.

--- tests/level.test.js
+++ tests/level.test.js
@@ -3,16 +3,17 @@
 // Dependencies
 // --------------------------------------------------------
 
-const chai = require('chai');
+const { expect } = require('chai');
+
+// Subject
+// --------------------------------------------------------
+
+const Level = require('../src/level');
 
 // --------------------------------------------------------
 
 describe('class Level', function ()
 {
-	const Level = require('../src/level');
-
-	// -------------------------------------------------------
-
 	describe('constructor(name, severity)', function ()
 	{
 		it('shall set Level#name to `name`', function ()
@@ -21,10 +22,8 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Assert.
-			chai.expect(level.name).to.equal('TRACE');
+			expect(level.name).to.equal('TRACE');
 		});
-
-		// ------------------------------------------------------
 
 		it('shall set Level#severity to `severity`', function ()
 		{
@@ -32,11 +31,9 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Assert.
-			chai.expect(level.severity).to.equal(10);
+			expect(level.severity).to.equal(10);
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#name', function ()
 	{
@@ -46,15 +43,13 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Act & Assert.
-			chai.expect(function ()
+			expect(function ()
 			{
 				level.name = 'DEBUG';
 
 			}).to.throw(TypeError);
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#severity', function ()
 	{
@@ -64,15 +59,13 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Act & Assert.
-			chai.expect(function ()
+			expect(function ()
 			{
 				level.severity = 15;
 
 			}).to.throw(TypeError);
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#toString()', function ()
 	{
@@ -82,13 +75,11 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Assert.
-			chai.expect(
+			expect(
 				level.toString()
 			).to.equal('TRACE');
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#toJSON()', function ()
 	{
@@ -98,46 +89,38 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Assert.
-			chai.expect(
+			expect(
 				level.toString()
 			).to.equal('TRACE');
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('.toLevel(levelToTranslate, defaultLevel = null)', function ()
 	{
 		it('shall return the pre-defined level with `levelToTranslate` as its name', function ()
 		{
 			// Act & Assert.
-			chai.expect(
+			expect(
 				Level.toLevel('ERROR')
 			).to.equal(Level.ERROR);
 		});
 
-		// ------------------------------------------------------
-
 		it('shall return `defaultLevel` if no pre-defined level exists with `levelToTranslate` as its name', function ()
 		{
 			// Act & Assert.
-			chai.expect(
+			expect(
 				Level.toLevel('TRACE', Level.INFO)
 			).to.equal(Level.INFO);
 		});
 
-		// ------------------------------------------------------
-
 		it('shall return `null` if no pre-defined level exists with `levelToTranslate` as its name and `defaultLevel` was not provided', function ()
 		{
 			// Act & Assert.
-			chai.expect(
+			expect(
 				Level.toLevel('TRACE')
 			).to.equal(null);
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#isEqualTo(level)', function ()
 	{
@@ -147,14 +130,12 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isEqualTo(
 					new Level('DEBUG', 10)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is not equal to that of `level`', function ()
 		{
@@ -162,15 +143,13 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isEqualTo(
 					new Level('TRACE', 15)
 				)
 			).to.be.false;
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#isGreaterThan(level)', function ()
 	{
@@ -180,14 +159,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThan(
 					new Level('TRACE', 5)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is equal to that of `level`', function ()
 		{
@@ -195,14 +172,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThan(
 					new Level('TRACE', 10)
 				)
 			).to.be.false;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is not greater than that of `level`', function ()
 		{
@@ -210,15 +185,13 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 5);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThan(
 					new Level('DEBUG', 10)
 				)
 			).to.be.false;
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#isGreaterThanOrEqualTo(level)', function ()
 	{
@@ -228,14 +201,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThanOrEqualTo(
 					new Level('TRACE', 5)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `true` when the level severity is equal to that of `level`', function ()
 		{
@@ -243,14 +214,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThanOrEqualTo(
 					new Level('TRACE', 10)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is less than that of `level`', function ()
 		{
@@ -258,15 +227,13 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 5);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isGreaterThanOrEqualTo(
 					new Level('DEBUG', 10)
 				)
 			).to.be.false;
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#isLessThan(level)', function ()
 	{
@@ -276,14 +243,12 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 5);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThan(
 					new Level('DEBUG', 10)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is equal to that of `level`', function ()
 		{
@@ -291,14 +256,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThan(
 					new Level('TRACE', 10)
 				)
 			).to.be.false;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is not less than that of `level`', function ()
 		{
@@ -306,15 +269,13 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThan(
 					new Level('TRACE', 5)
 				)
 			).to.be.false;
 		});
 	});
-
-	// -------------------------------------------------------
 
 	describe('#isLessThanOrEqualTo(level)', function ()
 	{
@@ -324,14 +285,12 @@ describe('class Level', function ()
 			let level = new Level('TRACE', 5);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThanOrEqualTo(
 					new Level('DEBUG', 10)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `true` when the level severity is equal to that of `level`', function ()
 		{
@@ -339,14 +298,12 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThanOrEqualTo(
 					new Level('TRACE', 10)
 				)
 			).to.be.true;
 		});
-
-		// ------------------------------------------------------
 
 		it('shall return `false` when the level severity is greater than that of `level`', function ()
 		{
@@ -354,7 +311,7 @@ describe('class Level', function ()
 			let level = new Level('DEBUG', 10);
 
 			// Act & Assert.
-			chai.expect(
+			expect(
 				level.isLessThanOrEqualTo(
 					new Level('TRACE', 5)
 				)
@@ -362,135 +319,144 @@ describe('class Level', function ()
 		});
 	});
 
-	// -------------------------------------------------------
-
-	context('has some levels pre-defined:', function ()
+	describe('.ALL', function ()
 	{
-		describe('.ALL', function ()
+		it('shall be a predefined level with the name `ALL`', function ()
 		{
-			it('that shall be the least severe level that can be defined', function ()
-			{
-				// Assert.
-				chai.expect(Level.ALL).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.ALL).to.be.instanceof(Level);
 
-				// Assert.
-				chai.expect(Level.ALL.name).to.equal('ALL');
-
-				// Assert.
-				chai.expect(Level.ALL.severity).to.equal(Number.MIN_VALUE);
-			});
+			// Assert.
+			expect(Level.ALL.name).to.equal('ALL');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.DEBUG', function ()
+		it('shall be the least severe level that can be defined', function ()
 		{
-			it('that shall be more severe than Level.ALL but less severe than Level.INFO', function ()
-			{
-				// Assert.
-				chai.expect(Level.DEBUG).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.ALL.severity).to.equal(Number.MIN_VALUE);
+		});
+	});
 
-				// Assert.
-				chai.expect(Level.DEBUG.name).to.equal('DEBUG');
+	describe('.DEBUG', function ()
+	{
+		it('shall be a predefined level with the name `DEBUG`', function ()
+		{
+			// Assert.
+			expect(Level.DEBUG).to.be.instanceof(Level);
 
-				// Assert.
-				chai.expect(
-					Level.DEBUG.severity > Level.ALL.severity && Level.DEBUG.severity < Level.INFO.severity
-				).to.be.true;
-			});
+			// Assert.
+			expect(Level.DEBUG.name).to.equal('DEBUG');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.INFO', function ()
+		it('shall be more severe than Level.ALL but less severe than Level.INFO', function ()
 		{
-			it('that shall be more severe than Level.DEBUG but less severe than Level.WARN', function ()
-			{
-				// Assert.
-				chai.expect(Level.INFO).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.DEBUG.severity).to.be.above(Level.ALL.severity);
 
-				// Assert.
-				chai.expect(Level.INFO.name).to.equal('INFO');
+			// Assert.
+			expect(Level.DEBUG.severity).to.be.below(Level.INFO.severity);
+		});
+	});
 
-				// Assert.
-				chai.expect(
-					Level.INFO.severity > Level.DEBUG.severity && Level.INFO.severity < Level.WARN.severity
-				).to.be.true;
-			});
+	describe('.INFO', function ()
+	{
+		it('shall be a predefined level with the name `INFO`', function ()
+		{
+			// Assert.
+			expect(Level.INFO).to.be.instanceof(Level);
+
+			// Assert.
+			expect(Level.INFO.name).to.equal('INFO');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.WARN', function ()
+		it('that shall be more severe than Level.DEBUG but less severe than Level.WARN', function ()
 		{
-			it('that shall be more severe than Level.INFO but less severe than Level.ERROR', function ()
-			{
-				// Assert.
-				chai.expect(Level.WARN).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.INFO.severity).to.be.above(Level.DEBUG.severity);
 
-				// Assert.
-				chai.expect(Level.WARN.name).to.equal('WARN');
+			// Assert.
+			expect(Level.INFO.severity).to.be.below(Level.WARN.severity);
+		});
+	});
 
-				// Assert.
-				chai.expect(
-					Level.WARN.severity > Level.INFO.severity && Level.WARN.severity < Level.ERROR.severity
-				).to.be.true;
-			});
+	describe('.WARN', function ()
+	{
+		it('shall be a predefined level with the name `WARN`', function ()
+		{
+			// Assert.
+			expect(Level.WARN).to.be.instanceof(Level);
+
+			// Assert.
+			expect(Level.WARN.name).to.equal('WARN');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.ERROR', function ()
+		it('that shall be more severe than Level.INFO but less severe than Level.ERROR', function ()
 		{
-			it('that shall be more severe than Level.WARN but less severe than Level.FATAL', function ()
-			{
-				// Assert.
-				chai.expect(Level.ERROR).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.WARN.severity).to.be.above(Level.INFO.severity);
 
-				// Assert.
-				chai.expect(Level.ERROR.name).to.equal('ERROR');
+			// Assert.
+			expect(Level.WARN.severity).to.be.below(Level.ERROR.severity);
+		});
+	});
 
-				// Assert.
-				chai.expect(
-					Level.ERROR.severity > Level.WARN.severity && Level.ERROR.severity < Level.FATAL.severity
-				).to.be.true;
-			});
+	describe('.ERROR', function ()
+	{
+		it('shall be a predefined level with the name `ERROR`', function ()
+		{
+			// Assert.
+			expect(Level.ERROR).to.be.instanceof(Level);
+
+			// Assert.
+			expect(Level.ERROR.name).to.equal('ERROR');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.FATAL', function ()
+		it('that shall be more severe than Level.WARN but less severe than Level.FATAL', function ()
 		{
-			it('that shall be more severe than Level.ERROR but less severe than Level.OFF', function ()
-			{
-				// Assert.
-				chai.expect(Level.FATAL).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.ERROR.severity).to.be.above(Level.WARN.severity);
 
-				// Assert.
-				chai.expect(Level.FATAL.name).to.equal('FATAL');
+			// Assert.
+			expect(Level.ERROR.severity).to.be.below(Level.FATAL.severity);
+		});
+	});
 
-				// Assert.
-				chai.expect(
-					Level.FATAL.severity > Level.ERROR.severity && Level.FATAL.severity < Level.OFF.severity
-				).to.be.true;
-			});
+	describe('.FATAL', function ()
+	{
+		it('shall be a predefined level with the name `FATAL`', function ()
+		{
+			// Assert.
+			expect(Level.FATAL).to.be.instanceof(Level);
+
+			// Assert.
+			expect(Level.FATAL.name).to.equal('FATAL');
 		});
 
-		// ------------------------------------------------------
-
-		describe('.OFF', function ()
+		it('that shall be more severe than Level.ERROR but less severe than Level.OFF', function ()
 		{
-			it('that shall be the most severe level that can be defined', function ()
-			{
-				// Assert.
-				chai.expect(Level.ALL).to.be.instanceof(Level);
+			// Assert.
+			expect(Level.FATAL.severity).to.be.above(Level.ERROR.severity);
 
-				// Assert.
-				chai.expect(Level.ALL.name).to.equal('ALL');
+			// Assert.
+			expect(Level.FATAL.severity).to.be.below(Level.OFF.severity);
+		});
+	});
 
-				// Assert.
-				chai.expect(Level.OFF.severity).to.equal(Number.MAX_VALUE);
-			});
+	describe('.OFF', function ()
+	{
+		it('shall be a predefined level with the name `OFF`', function ()
+		{
+			// Assert.
+			expect(Level.OFF).to.be.instanceof(Level);
+
+			// Assert.
+			expect(Level.OFF.name).to.equal('OFF');
+		});
+
+		it('that shall be the most severe level that can be defined', function ()
+		{
+			// Assert.
+			expect(Level.OFF.severity).to.equal(Number.MAX_VALUE);
 		});
 	});
 });
